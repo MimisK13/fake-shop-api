03a0868faef37b8a28808aad1467892b81ffba48
MimisK <mimisk88@gmail.com>
2025-04-26 09:13:01 +0300

    add gitignore

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('db.json');
+// const middlewares = jsonServer.defaults({
+//     static: 'public'
+// });
+// const routes = require('./routes.json');
+// const rewriter = jsonServer.rewriter(routes);
+//
+// server.use(middlewares);
+// server.use(rewriter);
+// server.use(router);
+//
+// const port = process.env.PORT || 3000;
+// server.listen(port, () => {
+//     console.log(`🚀 JSON Server running at http://localhost:${port}`);
+// });
+
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults({
     static: 'public'
 });
-const routes = require('./routes.json');
-const rewriter = jsonServer.rewriter(routes);
 
 server.use(middlewares);
-server.use(rewriter);
 server.use(router);
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-    console.log(`🚀 JSON Server running at http://localhost:${port}`);
-});
+module.exports = server; // 👈 Αντί να κάνεις server.listen, κάνε export
